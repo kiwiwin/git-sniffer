1f8e40f5fd2e03852154d03305fe47d77093f345
kiwi <kiwi.swhite.coder@gmail.com>
2012-11-19 22:48:02 +0800

    modify rakefile and git_sniffer required files

--- Rakefile
+++ Rakefile
@@ -1,1 +1,7 @@
 require "bundler/gem_tasks"
+
+task :default => [:spec]
+
+task :spec do
+	puts `rspec spec/*_spec.rb`
+end
--- lib/git_sniffer.rb
+++ lib/git_sniffer.rb
@@ -1,4 +1,7 @@
 require "git_sniffer/version"
+require "git_sniffer/base"
+require "git_sniffer/commit"
+require "git_sniffer/blob"
 
 module GitSniffer
   # Your code goes here...
