5666c038b19fb3d8025b6ebdd1cc183f0dc24881
kiwi <kiwi.swhite.coder@gmail.com>
2012-11-21 21:11:25 +0800

    add blob hook

--- lib/git_sniffer/hook.rb
+++ lib/git_sniffer/hook.rb
@@ -5,22 +5,50 @@ module GitSniffer
 		def initialize(base)
 			@base = base
 			@commit_hooks = {}
+			@blob_hooks = {}
 		end
 
 		def add_commit_hook(hook_name, hook_proc)
 			@commit_hooks[hook_name] = hook_proc
 		end
 
-		def run_hook
-			@commit_results = @base.commits.collect do |commit|
-				@commit_hooks.inject({}) do |res, hook|
-					res[hook[0]] = hook[1].call(commit); res
+		def add_blob_hook(hook_name, hook_proc)
+			@blob_hooks[hook_name] = hook_proc
+		end
+
+		def run
+			run_blobs
+			run_commits
+		end
+
+		def run_blobs
+			@blob_results ||= {}			
+			@base.blobs.each do |blob|
+				@blob_results[blob.sha] = @blob_hooks.inject({}) do |res, hook|
+					res[hook[0]] = hook[1].call(blob); res
+				end
+			end
+		end
+
+		def run_commits
+			@commit_results ||= {} 
+			@base.commits.each do |commit|
+				@commit_results[commit.sha] = @commit_hooks.inject({}) do |res, hook|
+					res[hook[0]] = hook[1].call(self, commit); res
 				end
 			end
 		end
 
 		def all_commit_result
 			@commit_results
+		end
+
+		def all_blob_result
+			@blob_results
+		end
+
+		def blob_result(sha, name)
+			@blob_results[sha][name]
 		end
 	end
 end
--- spec/hook_spec.rb
+++ spec/hook_spec.rb
@@ -8,17 +8,44 @@ describe GitSniffer::Hook do
 			end	
 
 			it "get all blobs size" do
-				@hook.add_commit_hook(:blobs, lambda { |commit| commit.blobs.size } )
-				@hook.run_hook
-				@hook.all_commit_result.should == [{:blobs => 3},{:blobs => 3},{:blobs => 2}]
+				@hook.add_commit_hook(:blobs, lambda { |hook, commit| commit.blobs.size } )
+				@hook.run
+				@hook.all_commit_result.should ==  {"c025dce424130b546754eb391a13eb601c4a243c"=>{:blobs=>3},
+																						"00cb610e642d0fac84ad4dac479b98ef447099cd"=>{:blobs=>3},
+																						"e9b02fdf95aa827c0bb2c244622120886a452bab"=>{:blobs=>2}} 
 			end		  
 
 			it "get all messages" do
-				@hook.add_commit_hook(:message, lambda { |commit| commit.message } )
-				@hook.run_hook
-				@hook.all_commit_result.should == [{:message => "say bye after say hello"},
-																					 {:message => "bye commit"},
-																					 {:message => "hello commit"}]
+				@hook.add_commit_hook(:message, lambda { |hook, commit| commit.message } )
+				@hook.run
+				@hook.all_commit_result.should == {
+																	"c025dce424130b546754eb391a13eb601c4a243c"=>{:message=>"say bye after say hello"}, 
+																	"00cb610e642d0fac84ad4dac479b98ef447099cd"=>{:message=>"bye commit"},
+																	"e9b02fdf95aa827c0bb2c244622120886a452bab"=>{:message=>"hello commit"}} 
+			end
+
+			it "get lines for each file" do
+				@hook.add_blob_hook(:lines, lambda { |blob| blob.content.split("\n").size } )
+				@hook.run
+				@hook.all_blob_result.should == {	"473e66f56cf9dcb2b875961937148e159b70d2b7"=>{:lines=>19},
+ 																					"f050e6b7dfe73d6af9d867e3622c0cae3e818207"=>{:lines=>15},
+																				  "f815e728173402ce9c19b090e5b85f93c5c422bf"=>{:lines=>7},
+																					"b909f7f1c27fc2ac344d3abda0e705645f556180"=>{:lines=>12},
+																					"6b468b62a9884e67ca19b673f8e14e1931d65036"=>{:lines=>1}}
+			end
+
+			it "get lines for each commit" do
+				@hook.add_blob_hook(:lines, lambda { |blob| blob.content.split("\n").size } )
+				commit_proc = Proc.new do |hook, commit|
+					commit.blobs.inject(0) do |res, blob|
+						res += hook.blob_result(blob.sha, :lines)
+					end
+				end
+				@hook.add_commit_hook(:lines, commit_proc)
+				@hook.run
+				@hook.all_commit_result.should == {"c025dce424130b546754eb391a13eb601c4a243c"=>{:lines=>32},
+ 																					 "00cb610e642d0fac84ad4dac479b98ef447099cd"=>{:lines=>23},
+																					 "e9b02fdf95aa827c0bb2c244622120886a452bab"=>{:lines=>8}} 
 			end
 		end
 end
