62f9f6003a1991cb0aaeae8753806b45a8c4ea9c
kiwi <kiwi.swhite.coder@gmail.com>
2012-11-27 15:42:04 +0800

    simplify hook impl

--- lib/git_sniffer/hook.rb
+++ lib/git_sniffer/hook.rb
@@ -7,35 +7,14 @@ module GitSniffer
 			base.extend(ClassMethods)
 		end
 
-		def eigenclass
-			class << self; self; end
-		end
-
-		def method_missing(method, *args, &block)
-			if method =~ /hook_(.+)/
-				eigenclass.instance_exec($1) do |attr|
+		module ClassMethods
+			def add_hook(name, &block)
+				class_exec(name, block) do |attr, callback|
 					lazy_reader "hook_#{attr}".to_sym
 					define_method "lazy_hook_#{attr}_source" do
-						self.class.get_hook(attr.to_sym).call self
+						callback.call self
 					end
 				end
-				return send method
-			end
-			super
-		end
-
-		module ClassMethods
-			def get_hook(name)
-				@hooks[name]
-			end
-
-			def add_hook(name, &block)
-				@hooks ||= {}
-				@hooks[name] = block
-			end
-
-			def remove_hook(name)
-				@hooks.delete(name)
 			end
 		end
 	end
